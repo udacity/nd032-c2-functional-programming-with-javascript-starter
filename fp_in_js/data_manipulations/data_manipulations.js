79b742d52865858157bb9875a5672c36cc371d67
christa-cheung <54732090+christa-cheung@users.noreply.github.com>
2020-07-09 16:36:10 -0700

    Create data_manipulations.js

---
+++ fp_in_js/data_manipulations/data_manipulations.js
@@ -0,0 +1,17 @@
+// The object in the nasa_near_earth_object_API.txt is a copy of real API response from the NASA Near-Earth Object API. 
+// Find the following from the API:
+
+// 1. How many near-earth objects did NASA register for the date of the search? Return the asteroid count
+
+
+// Hazardous -----------------------------------------------
+// 2. A list of all objects (their id, name, max size in miles, and closest approach in miles) that are labeled potentially hazardous
+
+
+// Too Close for Comfort -----------------------------------
+// 3. A list of all objects (their id, name, max size in miles, and closest approach in miles) that have a miss_distance of less than 900,000 miles
+
+
+// Alert ---------------------------------------------------
+// 4. Of all the near-earth objects for this date, find the time that the asteroid with the nearest miss will be closest to earth. 
+
