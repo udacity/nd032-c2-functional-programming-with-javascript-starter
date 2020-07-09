f7c62269eba51cb64750cb401215b4e5ad0cfcda
christa-cheung <54732090+christa-cheung@users.noreply.github.com>
2020-07-09 16:24:14 -0700

    Update object_freeze.js
    added notes on strict mode

--- functional_js_syntax/object_freeze.js
+++ functional_js_syntax/object_freeze.js
@@ -2,6 +2,12 @@
 // OBJECT FREEZE EXAMPLE from Video
 // ----------------------------------------------------------
 
+// Note here that we must run Javascript in strict mode in order to see the error correctly
+// Node runs in strict mode automatically, as do many online code editors, so if you are running these examples in the terminal or online you probably won't need this
+// If you want to learn more about strict mode, here is a good article: https://www.geeksforgeeks.org/strict-mode-javascript/
+
+"use strict";
+
 const currentShow = {
     title: 'Dr. Who',
     seasons: 11,
