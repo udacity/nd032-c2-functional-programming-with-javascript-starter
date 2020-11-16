0686ded3b074060a739abf32c27e9c91c1fcd9e3
Sreejith V S <sreejithvs333@gmail.com>
2020-11-16 18:59:30 +0530

    write get api for rover data in backend

--- project/src/server/index.js
+++ project/src/server/index.js
@@ -35,4 +35,4 @@ app.get('/rover', async (req, res) => {
     }
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Mars dashboard app listening on port ${port}!`))
