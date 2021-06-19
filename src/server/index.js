c64d7529bd2c6770ddbc2fc42304fba8f469e986
Jason Wilkes <44591433+aScruffyMind@users.noreply.github.com>
2021-06-18 19:26:03 -0700

    Back and Forward buttons on photo navbar complete and functional. (#5)

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,6 @@
 // jshint esversion: 8
 
-require('dotenv').config();
+const dotenv = require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const fetch = require('node-fetch');
@@ -33,37 +33,36 @@ function daysBetween(start, end) {
     return Number(((end - start) / (24 * 60 * 60 * 1000)).toFixed(0)).toLocaleString('en-US');
 }
 
+// Rover constructor function
+function Rover(rover) {
+    this.name = rover.name;
+    this.launchDate = rover.launch_date;
+    this.landingDate = rover.landing_date;
+    this.status = rover.status;
+    this.maxDate = rover.max_date;
+    this.missionDuration = `${daysBetween(new Date(rover.landing_date), new Date(rover.max_date))} days`;
+    // this.recentPhotos = rover.photos[rover.photos.length - 1];
+    this.totalPhotos = Number(rover.total_photos).toLocaleString('en-US');
+}
+
 app.get('/rover/:name', async (req, res) => {
-    const rover = req.params.name;
     try {
-        let curiosity = await fetch(`https://api.nasa.gov/mars-photos/api/v1/manifests/${rover}/?api_key=${process.env.API_KEY}`)
+        await fetch(`https://api.nasa.gov/mars-photos/api/v1/manifests/${req.params.name}/?api_key=${process.env.API_KEY}`)
             .then(res => res.json())
-            .then(data => {               
-                const r = data.photo_manifest;
-                const rover = {};
-                rover.name = r.name;
-                rover.launchDate = r.launch_date;
-                rover.landingDate = r.landing_date;
-                rover.status = r.status;
-                rover.maxDate = r.max_date;
-                rover.missionDuration = `${daysBetween(new Date(r.landing_date), new Date(r.max_date))} days`;
-                rover.recentPhotos = r.photos[r.photos.length - 1];
-                rover.totalPhotos = Number(r.total_photos).toLocaleString('en-US');
-                res.send(rover);
-            });
+            .then(data => res.send(new Rover(data.photo_manifest)));
     } catch (err) {
-        console.log('error:', err);
+        console.log('error: ', err);
     }
 });
 
 app.get('/photos/:name/:date', async (req, res) => {
-    fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${req.params.name}/photos?earth_date=${req.params.date}&api_key=w6XsOPxbyoqvmDAiYnaaImupw6igfShp3E0XS7wC`)
-        .then(res => res.json())
-        .then(data => {
-            res.send(data);
-        });
-    
+    try {
+        await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${req.params.name}/photos?earth_date=${req.params.date}&api_key=w6XsOPxbyoqvmDAiYnaaImupw6igfShp3E0XS7wC`)
+            .then(res => res.json())
+            .then(data => res.send(data.photos.map(photo => ({id: photo.id, camera: photo.camera.name, img_src: photo.img_src, photo_date: photo.earth_date}))));
+    } catch (err) {
+        console.log('error: ', err);
+    }   
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
