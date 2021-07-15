b814fdb8e77c109b0f876e9fefc818e5a255a6a4
karpovska <oksanachv@gmail.com>
2021-07-15 09:51:21 +0200

    Added background and api requests

--- project/src/server/index.js
+++ project/src/server/index.js
@@ -14,6 +14,26 @@ app.use('/', express.static(path.join(__dirname, '../public')))
 
 // your API calls
 
+app.get('/apod', async (req, res) => {
+    try {
+        let manifests = await fetch(`https://api.nasa.gov/mars-photos/api/v1/manifests/Curiosity?api_key=${process.env.API_KEY}`)
+            .then(res => res.json())
+        res.send({ manifests })        
+    } catch (err) {
+        console.log('manifest loading error:', err);
+    }
+})
+
+app.get('/apod', async (req, res) => {
+    try {
+        let images = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${process.env.API_KEY}`)
+            .then(res => res.json())
+        res.send ({ images })        
+    } catch (err) {
+        console.log('images loading error:', err);
+    }
+})
+
 // example API call
 app.get('/apod', async (req, res) => {
     try {
