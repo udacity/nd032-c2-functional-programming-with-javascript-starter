704ad2c030d9aa3ca4dd8d79320c2493978f6d64
Shrividya <shrividyaranjanikn@gmail.com>
2021-09-02 21:52:56 -0500

    add changes to render rovers and rover gallery

--- project/src/public/client.js
+++ project/src/public/client.js
@@ -1,9 +1,9 @@
 
 let store = {
-    baseUrl: 'http://localhost:3000',
+    apiBaseUrl: 'http://localhost:3000',
     imageAssetsPath: '/assets/images',
     app: { title: "Mars Rover Exploration" },
-    rover_manifest_details: undefined,
+    rovers: undefined
 }
 
 const roverNames = ["Curiosity", "Opportunity", "Spirit"];
@@ -17,20 +17,7 @@ const updateStore = (store, newState) => {
 }
 
 const updateRovers = (store, newRoverState) => {
-    store = Object.assign(store, {rover_manifest_details: newRoverState});
-}
-
-const getMarsRovers = async (state) => {
-    let rover_manifest_details = state.rover_manifest_details ?? JSON.parse(localStorage.getItem("rover_manifest_details")) ?? [];
-
-    if (!rover_manifest_details || rover_manifest_details.length == 0) {
-        rover_manifest_details = (await Promise.all(roverNames.map(roverName => getRoverManifest(roverName)))).map(data => data.manifest_data);
-
-        localStorage.setItem("rover_manifest_details", JSON.stringify(rover_manifest_details));
-    }
-    
-    updateRovers(state, rover_manifest_details);
-    return rover_manifest_details;
+    store = Object.assign(store, {rovers: newRoverState});
 }
 
 const render = async (root, state) => {
@@ -38,13 +25,12 @@ const render = async (root, state) => {
     setButtonClickEventListeners(state)
 }
 
-
 // create content
 const App = async (state) => {
     let rovers = await getMarsRovers(state);
 
     return `
-        <header>${Greeting(store.app.title)}</header>
+        <header>${AppGreeting(store.app.title)}</header>
         <main>
             <section class='rover-cards'>
                 ${RenderNasaRovers(state, rovers)}
@@ -63,7 +49,7 @@ window.addEventListener('load', () => {
     render(root, store)
 })
 
-const Greeting = (title) => {
+const AppGreeting = (title) => {
     if (title) {
         return `
             Welcome to ${title}!
@@ -82,13 +68,21 @@ const RenderNasaRovers = (state, rovers) => {
 }
 
 const RenderRoverCard = (state, rover) => {
+
+    if (!rover) {
+        console.error("Rover is undefined");
+        return;
+    }
     return (`
-        <div class="card" style="width: 18rem;">
-            <img class="card-img-top" src='${state.imageAssetsPath}/${rover.name.toLowerCase()}_card_image.jpg' alt="${rover.name} image">
+        <div class="card col-sm-6 col-md-3">
+            <img class="card-img-top" height="200px" src='${state.imageAssetsPath}/${rover.name.toLowerCase()}_card_image.jpg' alt="${rover.name} image">
             <div class="card-body">
                 <h5 class="card-title">${rover.name}</h5>
-                <p class="card-text">
-                    Random Text
+                <p class="card-text rover-details">
+                    <span>Launch Date: ${rover.launch_date}</span>
+                    <span>Landing Date: ${rover.landing_date}</span>
+                    <span>Status: ${rover.status}</span>
+                    <span>Max Sol: ${rover.max_sol}</span>
                 </p>
                 <button type="button" id="btn-${rover.name}" class="btn btn-primary">View More Information</button>
             </div>
@@ -97,9 +91,9 @@ const RenderRoverCard = (state, rover) => {
 }
 
 const RenderGallery = () => {
-    return `(
+    return (`
         <div id="roverGalleryControls" class="carousel slide hide" data-ride="carousel">
-            <div class="carousel-inner"></div>
+            <div class="carousel-inner" style=" width:100%; height: 500px !important;"></div>
             <a class="carousel-control-prev" href="#roverGalleryControls" role="button" data-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span class="sr-only">Previous</span>
@@ -109,26 +103,35 @@ const RenderGallery = () => {
                 <span class="sr-only">Next</span>
             </a>
         </div>
-    )`
+    `)
 }
 
 const setButtonClickEventListeners = (state) => {
-    const rovers = state.rover_manifest_details
-    rovers.forEach(rover => {
+    const rovers = state.rovers
+    rovers.forEach((rover) => {
         document.getElementById(`btn-${rover.name}`).addEventListener('click', function() {
             let element = document.querySelector('#roverGalleryControls');
             element.classList.remove('hide');
-            element.firstChild.innerHTML = RenderRoverSlide(rover);
+            let carouselElement = document.querySelector('.carousel-inner');
+            carouselElement.innerHTML = RenderRoverSlides(rover);
         });
     });
 }
 
-const RenderRoverSlide = (rover, index) => {
-    return `(
-        <div class="carousel-item ${index == 1 ? 'active' : ''}">
-            <img class="d-block w-100" src="" alt="${rover.name} slide">
-        </div>
-    )`
+const RenderRoverSlides = (rover) => {
+    let sliderInnerHtml = "";
+    rover.photos.forEach((photo, index) => {
+        sliderInnerHtml += `
+            <div class="carousel-item ${index == 1 ? 'active' : ''}">
+                <img class="d-block w-100" src="${photo.image}" alt="${rover.name} slide">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Camera used: ${photo.camera}</h5>
+                    <p>${photo.photo_date}</p>
+                </div>
+            </div>
+        `;
+    })
+    return sliderInnerHtml;
 }
 
 // Example of a pure function that renders infomation requested from the backend
@@ -159,12 +162,42 @@ const ImageOfTheDay = (apod) => {
     }
 }
 
+const getMarsRovers = async (state) => {
+    let rovers = state.rovers ?? JSON.parse(localStorage.getItem("rovers")) ?? [];
+
+    if (!rovers || rovers.length == 0) {
+        rovers = (await Promise.all(roverNames.map(async roverName => {
+            
+            const result = await Promise.all([getRoverInfo(roverName), getRoverPhotos(roverName)])
+            const roverInfo = result[0];
+            const roverPhotos = result[1];
+
+            roverInfo.photos = roverPhotos;
+
+            return roverInfo;
+        }))).map(rover => rover);
+
+        localStorage.setItem("rovers", JSON.stringify(rovers));
+    }
+    
+    updateRovers(state, rovers);
+    return rovers;
+}
+
+
 // API Calls
-const getRoverManifest = async (rover_name) => {
-    let roverManifestData = await fetch(`${store.baseUrl}/manifests/${rover_name}`)
+const getRoverInfo = async (rover_name) => {
+    let roverInfo = await fetch(`${store.apiBaseUrl}/manifests/${rover_name}`)
         .then(res => res.json())
 
-    return roverManifestData;
+    return roverInfo.mars_rover;
+}
+
+const getRoverPhotos = async (rover_name) => {
+    let roverPhotos = await fetch(`${store.apiBaseUrl}/photos/${rover_name}`)
+            .then(res => res.json())
+
+    return roverPhotos;
 }
 
 const getImageOfTheDay = (state) => {
--- project/src/server/index.js
+++ project/src/server/index.js
@@ -8,6 +8,8 @@ const app = express()
 const port = 3000
 const nasaApiBaseUrl = `https://api.nasa.gov`;
 const manifestsUri = `/mars-photos/api/v1/manifests/`;
+const roverPhotosUri = `/mars-photos/api/v1/rovers/`;
+const photo_count = 15;
 
 
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -20,12 +22,32 @@ app.use('/', express.static(path.join(__dirname, '../public')))
 
 app.get('/manifests/:rover_name', async (req, res) => {
     try {
-        let manifest_data = await fetch(`${nasaApiBaseUrl}${manifestsUri}/${req.params.rover_name}?api_key=${process.env.API_KEY}`)
+        let mars_rover = await fetch(`${nasaApiBaseUrl}${manifestsUri}${req.params.rover_name}?api_key=${process.env.API_KEY}`)
             .then(res => res.json())
             .then(data => data['photo_manifest']);
-        res.send({manifest_data});
+        res.send({mars_rover});
     }
     catch (err) {
+        console.error("error: ", err);
+    }
+})
+
+app.get('/photos/:rover_name/', async (req, res) => {
+    try {
+        const endpoint = `${nasaApiBaseUrl}${roverPhotosUri}${req.params.rover_name}/photos?sol=1000&api_key=${process.env.API_KEY}&page=1`;
+        let photos = await fetch(endpoint)
+                .then(res => res.json())
+                .then(data => data.photos.map(photo => {
+                    return {
+                        image: photo.img_src,
+                        camera: photo.camera.full_name,
+                        photo_date: photo.earth_date ?? 'Date unavailable'
+                    }
+                }))
+                .then(photos => photos.length > photo_count ? photos.slice(0, photo_count+1) : photos)
+        res.send(photos)
+    }
+    catch(err) {
         console.error("error: ", err);
     }
 })
