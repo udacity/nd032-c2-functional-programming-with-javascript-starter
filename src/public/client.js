73118198306384d77662450eeaed309e7d240969
benjaminhyw <benjaminhyw@gmail.com>
2021-09-26 17:34:01 -0700

    Update spacing

--- src/public/client.js
+++ src/public/client.js
@@ -51,32 +51,32 @@ const App = (state) => {
   let { rovers, apod } = state;
 
   return `
-        <header>
-          <nav class="rover-nav">
-            ${navMenu()}
-          </nav>
-        </header>
-        <main>
-            <section>
-                <div id="content" style="display:none">
-                  ${renderData(state)}
-                  <div id="roverPhotos">
-                    ${getRoverImage(state)}
-                  </div>
-                </div>
-                <p>Here is an example section.</p>
-                <p>
-                    One of the most popular websites at NASA is the Astronomy Picture of the Day. In fact, this website is one of
-                    the most popular websites across all federal agencies. It has the popular appeal of a Justin Bieber video.
-                    This endpoint structures the APOD imagery and associated metadata so that it can be repurposed for other
-                    applications. In addition, if the concept_tags parameter is set to True, then keywords derived from the image
-                    explanation are returned. These keywords could be used as auto-generated hashtags for twitter or instagram feeds;
-                    but generally help with discoverability of relevant imagery.
-                </p>
-                ${ImageOfTheDay(apod)}
-            </section>
-        </main>
-        <footer></footer>
+    <header>
+      <nav class="rover-nav">
+        ${navMenu()}
+      </nav>
+    </header>
+    <main>
+      <section>
+        <div id="content" style="display:none">
+          ${renderData(state)}
+          <div id="roverPhotos">
+            ${getRoverImage(state)}
+          </div>
+        </div>
+        <p>Here is an example section.</p>
+        <p>
+          One of the most popular websites at NASA is the Astronomy Picture of the Day. In fact, this website is one of
+          the most popular websites across all federal agencies. It has the popular appeal of a Justin Bieber video.
+          This endpoint structures the APOD imagery and associated metadata so that it can be repurposed for other
+          applications. In addition, if the concept_tags parameter is set to True, then keywords derived from the image
+          explanation are returned. These keywords could be used as auto-generated hashtags for twitter or instagram feeds;
+          but generally help with discoverability of relevant imagery.
+        </p>
+        ${ImageOfTheDay(apod)}
+      </section>
+    </main>
+    <footer></footer>
     `;
 };
 
@@ -96,7 +96,9 @@ window.addEventListener("load", () => {
 
 const renderData = (state) => {
   if (state.selectedRover) {
-    return `<h3>${state.selectedRover}</h3>`;
+    return `
+      <h3>${state.selectedRover}</h3>
+    `;
   }
 
   return `<h1>PLACEHOLDER!</h1>`;
