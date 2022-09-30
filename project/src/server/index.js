f7d9dbe3bd488587f78fc75f72bbf180eb450890
Nick Cunningham <nic1571@gmail.com>
2022-09-30 16:29:06 -0600

    Test Express get with NASA API key

--- project/src/server/index.js
+++ project/src/server/index.js
@@ -7,22 +7,22 @@ const path = require('path')
 const app = express()
 const port = 3000
 
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-
 app.use('/', express.static(path.join(__dirname, '../public')))
 
-// your API calls
+app.get('/:rover', async (req, res) => {
+    const rover = req.params.rover
+    const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?earth_date=2015-6-3&api_key=${process.env.API_KEY}`
 
-// example API call
-app.get('/apod', async (req, res) => {
     try {
-        let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
+        // let image = await fetch(url)
+        // let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
+        let image = await fetch(url)
             .then(res => res.json())
         res.send({ image })
     } catch (err) {
         console.log('error:', err);
     }
 })
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
